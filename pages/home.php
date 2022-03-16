5c2bdc38085cd970ae2edc7932c6c25c8243adf9
André da Maia <andre.jr91@gmail.com>
2022-03-16 14:40:24 -0300

    beta v1.0

--- pages/home.php
+++ pages/home.php
@@ -1,26 +1,64 @@
-<div class="boilerplate">
+<div class="banner">
+    <img src="<?php echo $pageurl; ?>/public/images/banner-1.jpg" alt="" />
+
     <div class="container">
-        <div class="row d-flex align-items-center">
-            <div class="col-12 col-lg-4 col-lg-offset-2 text-center main">
-                <h2 style="opacity: 0;">Pronto para começar.</h2>
+        <div class="row">
+            <div class="col-8 col-lg-8">
+                <div class="title-banner">
+                    <h1>A combinação perfeita <br/>de tecnologia, estilo<br/> e proteção</h1>
 
-                <p>Boilerplate preparado para ser utilizado como base de desenvolvimento: <br /> SASS, Webpack, Three.js, ES6 modules, PHP</p>
-
-                <div class="module-tip">
-                    <span class="tip-icon"><svg width="24" height="24" viewBox="0 0 24 24" fill="#ffffff80" stroke="#11111190" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-box"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg></span>
-                    <span class="tip-title">Utilize <span>`components`</span> para os módulos reutilizáveis</span>
-                </div>
-
-                <div class="module-tip">
-                    <span class="tip-icon"><svg width="24" height="24" viewBox="0 0 24 24" fill="#ffffff80" stroke="#11111190" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-folder"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg></span>
-                    <span class="tip-title">Em <span>`pages`</span> são os arquivos que controlam a navegação</span>
-                </div>
-
-                <div class="module-tip">
-                    <span class="tip-icon"><svg width="24" height="24" viewBox="0 0 24 24" fill="#ffffff80" stroke="#11111190" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-archive"><polyline points="21 8 21 21 3 21 3 8"></polyline><rect x="1" y="3" width="22" height="5"></rect><line x1="10" y1="12" x2="14" y2="12"></line></svg></span>
-                    <span class="tip-title">Todos os resources devem ficar na pasta <span>`src`</span></span>
+                    <p>As películas automotivas foram criadas para <br/> dar mais estilo e sofisticação ao seu carro.</p>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="banner-options">
+        <a href="#" class="options-produtos">
+            <svg width="27" height="25" viewBox="0 0 27 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.901 20.0303L7.90237 20.5088C7.90237 22.2949 6.152 23.7359 4.36312 23.7359C2.57562 23.7359 1.125 22.2853 1.125 20.4992V14.0422C1.125 12.2547 2.57562 10.8054 4.36312 10.8054L15.3329 10.8192C16.9732 10.8192 18.3029 12.1502 18.3029 13.7919L18.2987 22.7913" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M7.62739 23.5062L7.60126 5.11286C7.60126 2.98024 9.33101 1.25049 11.4636 1.25049H22.0278C24.1618 1.25049 25.8915 2.98024 25.8915 5.11286V19.879C25.8915 22.0116 24.1618 23.74 22.0278 23.74H4.21051" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+
+            <strong>Conheça melhor</strong>
+
+            <p>Nossos produtos</p>
+        </a>
+
+        <a href="#" class="options-orcamento">
+            <svg width="36" height="24" viewBox="0 0 36 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M30.3744 16.0557H34.7394V4.83194H29.8556L26.2956 1.27319H18.5794C18.5794 1.27319 14.2581 5.16069 13.1144 6.30319C11.9719 7.44694 12.1331 8.98569 12.9031 9.80694C13.8906 10.8594 15.3656 10.8182 16.4444 9.89694C17.3394 9.13319 20.6069 6.13194 20.6069 6.13194" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M13.2281 1.27319H10.1456L6.57686 4.69444H1.93561V16.0557H6.33436C6.33436 16.0557 11.9344 21.3007 12.4306 21.7019C14.8156 23.6257 18.5281 21.5282 19.3244 20.5932C26.7381 19.9007 27.8806 16.1594 26.1144 14.4969C24.3481 12.8344 22.0569 10.4832 22.0569 10.4832" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+
+            <strong>Inicie um orçamento</strong>
+
+            <p>Sem compromisso</p>
+        </a>
+
+        <a href="#" class="video">
+            <img src="<?php echo $pageurl; ?>/public/images/banner-video.jpg" alt="" />
+
+            <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="80" height="80" rx="40" fill="white"/>
+                <path d="M34 33L34 46L49 39.5L34 33Z" fill="#F12D2F"/>
+            </svg>
+        </a>
+    </div>
+</div>
+
+<div class="area-white pelicula-description">
+    <img src="<?php echo $pageurl; ?>/public/images/area-white-effect.png" alt="" />
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-8 col-lg-8">
+                <h2>Nossas películas mantém o desempenho<br/> ao longo do tempo. Com a mais alta<br/> tecnologia óptica e de rejeição de calor.<br/> Sem dúvida, sua melhor escolha.</h2>
+            </div>
+        </div>
+    </div>
+
+    <svg width="497" height="238" viewBox="0 0 497 238" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path opacity="0.8" d="M103.791 0C187.179 124.749 393.283 110.063 469.722 82.3406C484.5 148.5 475.5 105 497 218.494C205.143 285.027 45.1684 165.59 0 96.2854C53.8916 46.4826 6.65329 89.645 103.791 0Z" fill="#F12D2F"/>
+    </svg>
 </div>
--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -7,12 +7,12 @@ const pageurl = document.querySelector('body').id
 if(pageurl == 'home'){
     const scroll = new Scroll()
 
-    gsap.ticker.add(() => { 
-        gsap.to('h2', {
-          y: -scroll.screenPosition/2,
-          autoAlpha: scroll.screenPosition/500,
-          ease: 'power2.out',
-          duration: 1.6
-        })
-    })
+    // gsap.ticker.add(() => { 
+    //     gsap.to('h2', {
+    //       y: -scroll.screenPosition/2,
+    //       autoAlpha: scroll.screenPosition/500,
+    //       ease: 'power2.out',
+    //       duration: 1.6
+    //     })
+    // })
 }
